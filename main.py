cde9a830cfc4360ffd717c5c7388fce8e54da39d
Daniel Zadva Jnr <danielzadva@gmail.com>
2024-11-15 23:18:22 +0100

    user creation endpoint completed

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-
+"""FastAPI application with Middleware"""
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, EmailStr
+import time
 
 class User(BaseModel):
     """Model for User"""
     first_name: str
     last_name: str
     age: int
-    email: str
+    email: EmailStr
     height: float
 
+#Users in memory database
 users = []
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"HOME": "Welcome to Sign Up API Home Page"}
+#CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.post("/signup")
-def signup(user: User):
+#Logger Middleware
+@app.middleware("http")
+async def log_requests_time(request: Request, call_next):
+    """Middleware to log requests time"""
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    print(f"Request: {request.method} {request.url} completed in {process_time:.1f} seconds")
+    return response
+
+
+@app.post("/users", status_code=201)
+def create_user(user: User):
     """Signup a new user"""
+    for i, j in enumerate(users):
+        if user == users[i]:
+            raise HTTPException(status_code=400, detail="User already exists")
     users.append(user)
     return {"User Added Successfully!\n": user}  
 
